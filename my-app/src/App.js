e3ba34359d16050ca479f46063c33e1ddcb8fbfa
minhdangnsp <dangnguyentranminh@gmail.com>
2023-06-26 16:56:14 +0700

    ch2.bải-15: Rendering list phần 3 - handel khi khi sử dụng map

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,24 +5,31 @@ import { YoutubeData } from "./YoutubeData";
 function App() {
   return (
     <div className="youtube-list">
-      {YoutubeData.map((item, index) => (
-        <div>
-          <YoutubeItem
-            key={item.id} // key is not required, but when rendering list, it is recommended to have key
-            image={item.image}
-            avatar={item.avatar || item.image}
-            author={item.author}
-            title={item.title}
-          ></YoutubeItem>
-        </div>
-      ))}
+      {YoutubeData.map((item, index) => {
+        const newClass = "";
+        if (index === 1) newClass = "abc";
+
+        {/* const newClass = index === 1 ? "abc" : ""; */}
+        return (
+          <div>
+            <YoutubeItem
+              key={item.id} // key is not required, but when rendering list, it is recommended to have key
+              image={item.image}
+              avatar={item.avatar || item.image}
+              author={item.author}
+              title={item.title}
+              className={newClass}
+            ></YoutubeItem>
+          </div>
+        );
+      })}
     </div>
   );
 }
 
 function YoutubeItem(props) {
   return (
-    <div className="youtube-item">
+    <div className={`youtube-list ${props.className}`}>
       <div className="youtube-image">
         <img src={props.image} alt="" />
       </div>
