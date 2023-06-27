9562aa230b63aad4b1b2c38508e31697b766544d
minhdangnsp <dangnguyentranminh@gmail.com>
2023-06-27 16:35:55 +0700

    ch3.b36: tìm hiểu useReducer.

--- my-app/src/components/tictactoe/Game.js
+++ my-app/src/components/tictactoe/Game.js
@@ -1,8 +1,38 @@
-import React, { useState } from "react";
+import React, { useReducer, useState } from "react";
 import Board from "./Board";
 import "./GameStyles.css";
 import { calculateWinner } from "../../helplers";
 
+const initalState = {
+  board: Array(9).fill(null),
+  xIsNext: true,
+};
+
+const gameReducer = (state, action) => {
+  switch (action.type) {
+    case "CLICK": {
+      const { board, xIsNext } = state;
+      const { index, winner } = action.payload;
+      if (winner || board[index]) return state;
+      // clone state to not mutate the original state
+      const nextState = JSON.parse(JSON.stringify(state));
+      nextState.board[index] = xIsNext ? "X" : "O";
+      nextState.xIsNext = !xIsNext;
+      return nextState;
+    }
+    case "RESET": {
+      const nextState = JSON.parse(JSON.stringify(initalState));
+      nextState.board = Array(9).fill(null);
+      return nextState;
+    }
+
+    default:
+      console.log("Error");
+      break;
+  }
+  return state;
+};
+/** 
 const Game = () => {
   const [board, setBoard] = useState(Array(9).fill(null));
   const [xIsNext, setXIsNext] = useState(true); // this state is used to determine which player is next (x or o)
@@ -22,6 +52,27 @@ const Game = () => {
   return (
     <div>
       <Board cells={board} onClick={handelClick}></Board>
+      {winner && <div className="game-winner">Winner is {winner}</div>}
+      <button className="game-reset" onClick={handleResetGame}>
+        Reset game
+      </button>
+    </div>
+  );
+};
+*/
+
+const Game = () => {
+  const [state, dispatch] = useReducer(gameReducer, initalState);
+  const winner = calculateWinner(state.board);
+  const handelClick = (index) => {
+    dispatch({ type: "CLICK", payload: { index, winner } });
+  };
+  const handleResetGame = () => {
+    dispatch({ type: "RESET", payload: {} });
+  };
+  return (
+    <div>
+      <Board cells={state.board} onClick={handelClick}></Board>
       {winner && <div className="game-winner">Winner is {winner}</div>}
       <button className="game-reset" onClick={handleResetGame}>
         Reset game
