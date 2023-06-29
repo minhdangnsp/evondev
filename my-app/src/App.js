220fa6771a0e7c317cf9827003f1e8656d7989b8
minhdangnsp <dangnguyentranminh@gmail.com>
2023-06-29 13:32:49 +0700

    ch4.b51: Tìm hiểu Theme trong styled-components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,27 @@
 import "./App.css"; // import css file
-import Card from "./components/card/Card";
+import Card2 from "./components/card/Card2";
 import CardList from "./components/card/CardList";
 import { GlobalStyles } from "./GlobalStyles";
+import { ThemeProvider } from "styled-components";
+
+const theme = {
+  colors: {
+    blue: "#2979FF",
+  },
+  orange: "#FF6D3F",
+};
 
 // parent component
 function App() {
   return (
     <div>
-      <GlobalStyles></GlobalStyles>
-      <CardList>
-        {/* {Array(9)
-          .fill()
-          .map((_, index) => (
-            <Card></Card>
-          ))} */}
-        <Card secondary={true}></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-        <Card></Card>
-      </CardList>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles></GlobalStyles>
+        <CardList>
+          <Card2 secondary={true}></Card2>
+          <Card2></Card2>
+        </CardList>
+      </ThemeProvider>
     </div>
   );
 }
